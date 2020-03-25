a553eb5ee94f8dbed644d9a917b15a29609075eb
colon-ku <ksmtsho3@gmail.com>
2020-03-25 18:25:33 +0900

    159

---
+++ 159/d.cpp
@@ -0,0 +1,82 @@
+/*
+    飲んだ魔剤で家が建つ。
+    created at: 2020-03-25 17:35:55
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define rep(x, y) for (int x = 0; x < y; x++)
+#define MOD 1000000007
+
+typedef long long LL;
+typedef long double LD;
+
+bool compare_by_b(pair<int, int> a, pair<int, int> b)
+{
+    if (a.second != b.second) {
+        return a.second < b.second;
+    } else {
+        return a.first < b.first;
+    }
+}
+
+LL modpower(LL n, LL m, LL mod) {
+    if (m == 0) {
+        return 1;
+    } else if (m % 2 == 0) {
+        LL p = modpower(n, m/2, mod);
+        return p*p % mod;
+    } else {
+        return n*modpower(n, m-1, mod) % mod;
+    }
+}
+
+LL modinv(LL n, LL mod) {
+    return modpower(n, mod-2, mod);
+}
+
+LL modcombination(LL n, LL r, LL mod) {
+    LL ret = 1;
+
+    for (int i = 0; i < r; i++) {
+        ret *= (n-i);
+        ret %= mod;
+        ret *= modinv(i+1, mod);
+        ret % mod;
+    }
+
+    return ret % mod;
+}
+
+ostringstream oss_global;
+string s_global = oss_global.str();
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    map<int, int> memo;
+    for (int i = 0; i < n; i++) 
+        memo[a[i]]++;
+
+    LL total = 0;
+    for (const auto& x : memo) {
+        int v = x.second;
+        total += (LL)v*(v-1)/2;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int w = a[i];
+        int v = memo[w];
+
+        cout << total - v + 1 << endl;
+    }
+
+    return 0;
+}
