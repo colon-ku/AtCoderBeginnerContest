767d018759903377e9e1066364fa369d6c42bf29
colon-ku <ksmtsho3@gmail.com>
2020-04-08 11:09:34 +0900

    全探索で十分間に合う

---
+++ 95/b.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+#define INF 100000
+
+int n, x, m[INF], ans;
+
+void solve()
+{
+    ans = 0;
+
+    int mini = INF;
+    for (int i = 0; i < n; i++) {
+        int temp = m[i];
+        x -= temp;
+        if (temp < mini) mini = temp;
+    }
+    ans += n;
+
+    ans += x/mini;
+}
+
+int main()
+{
+    scanf("%d%d", &n, &x);
+    for (int i = 0; i < n; i++) {
+        int temp;
+        scanf("%d", &temp);
+        m[i] = temp;
+    }
+
+    solve();
+
+    cout << ans << endl;
+
+    return 0;
+}
---
+++ 95/c.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+#define INF 2000000000
+#define N 100000
+
+int a, b, c, x, y, ans;
+
+void solve()
+{
+    ans = INT_MAX;
+
+    for (int i = 0; i <= N; i++) {
+        int sum = 2*c*i;
+
+        sum += a*max(0, x-i);
+        sum += b*max(0, y-i);
+
+        if (sum < ans) ans = sum;
+    }
+}
+
+int main()
+{
+    scanf("%d%d%d%d%d", &a, &b, &c, &x, &y);
+
+    solve();
+
+    printf("%d\n", ans);
+
+    return 0;
+}
