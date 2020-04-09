0580355e038a655c2a670a4fc74379a779209167
colon-ku <ksmtsho3@gmail.com>
2020-04-09 16:06:48 +0900

    定数オーダーで計算可能

---
+++ 101/c.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#define MAX_N 100000
+
+int n, k, a[MAX_N];
+int ans;
+
+void solve()
+{
+    ans = ceil((float)(n-1)/(k-1));
+}
+
+int main()
+{
+    scanf("%d%d", &n, &k);
+    for (int i = 0; i < n; i++) {
+        int temp;
+        scanf("%d", &temp);
+        a[i] = temp;
+    }
+
+    solve();
+
+    cout << ans << endl;
+
+    return 0;
+}
