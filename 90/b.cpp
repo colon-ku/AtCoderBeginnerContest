4330efc14753aa1054e2b932c935e57d1e3d1432
colon-ku <ksmtsho3@gmail.com>
2020-04-10 18:49:43 +0900

    A,B埋め

---
+++ 90/b.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+int deci[5];
+
+bool per(int n)
+{
+    for (int i = 0; i < 5; i++) {
+        deci[4-i] = n%10;
+        n /= 10;
+    }
+
+    if (deci[0] == deci[4] && deci[1] == deci[3]) return true;
+    else return false;
+}
+
+void solve()
+{
+    int ans = 0;
+    for (int i = a; i <= b; i++) {
+        if (per(i)) ans++;
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    cin >> a >> b;
+
+    solve();
+
+    return 0;
+}
