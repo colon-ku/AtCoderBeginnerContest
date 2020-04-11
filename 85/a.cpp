3235e7539fe09b7c9d85170431e0d2ffefc6e4bd
colon-ku <ksmtsho3@gmail.com>
2020-04-11 17:47:55 +0900

    問題文通りのルールでない新ルールを定義してみると解ける

---
+++ 85/a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char s[10];
+
+void solve()
+{
+    s[3] = '8';
+    printf("%s\n", s);
+}
+
+int main()
+{
+    scanf("%s", &s);
+
+    solve();
+
+    return 0;
+}
