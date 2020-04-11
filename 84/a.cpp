1dea4a109995eb9a40c53ca7b137b0b7d0cfc85f
colon-ku <ksmtsho3@gmail.com>
2020-04-11 18:16:32 +0900

    Nの大きさに注意すればC問題でも愚直に実装するだけでよいものもある

---
+++ 84/a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int m;
+
+int main() {
+    scanf("%d", &m);
+    printf("%d\n", 48-m);
+    return 0;
+};
---
+++ 84/b.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int a, b;
+string s;
+
+void solve()
+{
+    int ok = 1;
+    for (int i = 0; i < a+b+1; i++) {
+        if (i == a && s[i] != '-' || i != a && s[i] == '-') {
+            ok = 0;
+        }
+    }
+
+    if (ok) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
+
+int main()
+{
+    cin >> a >> b >> s;
+
+    solve();
+
+    return 0;
+}
