a41a0d0d4904d30e26a2161228ccdc3e9b6b2bb7
colon-ku <ksmtsho3@gmail.com>
2020-04-26 20:58:10 +0900

    63-D途中まで

---
+++ 63/c.cpp
@@ -0,0 +1,119 @@
+/*
+    飲んだ魔剤で家が建つ。
+    created at: 2020-04-26 20:29:51
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define rev(x) greater<x>()
+#define MOD 1000000007
+#define INF 1000000000
+
+typedef long long LL;
+typedef long double LD;
+
+bool compare_by_b(pair<int, int> a, pair<int, int> b)
+{
+    if (a.second != b.second) {
+        return a.second < b.second;
+    } else {
+        return a.first < b.first;
+    }
+}
+
+LL modpower(LL n, LL m, LL mod)
+{
+    if (m == 0) {
+        return 1;
+    } else if (m % 2 == 0) {
+        LL p = modpower(n, m/2, mod);
+        return p*p % mod;
+    } else {
+        return n*modpower(n, m-1, mod) % mod;
+    }
+}
+
+LL modinv(LL n, LL mod)
+{
+    return modpower(n, mod-2, mod);
+}
+
+LL modcombination(LL n, LL r, LL mod)
+{
+    LL ret = 1;
+
+    for (int i = 0; i < r; i++) {
+        ret *= (n-i);
+        ret %= mod;
+        ret *= modinv(i+1, mod);
+        ret % mod;
+    }
+
+    return ret % mod;
+}
+
+int gcd(int k, int l)
+{
+    if (l > 0) return gcd(l, k%l);
+    else return k;
+}
+
+struct UnionFind
+{
+    vector<int> par;
+
+    UnionFind(int n) : par(n) {
+        for (int i = 0; i < n; i++) par[i] = i;
+    }
+
+    int root(int x) {
+        if (par[x] == x) return x;
+        return par[x] = root(par[x]);
+    }
+
+    void unite(int x, int y) {
+        int rx = root(x);
+        int ry = root(y);
+        if (rx == ry) return;
+        par[rx] = ry;
+    }
+
+    bool same(int x, int y) {
+        int rx = root(x);
+        int ry = root(y);
+        return rx == ry;
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> s(n);
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+
+    int sum = 0;
+    vector<int> und;
+    for (int i = 0; i < n; i++) {
+        sum += s[i];
+        if (s[i] % 10 != 0) {
+            und.push_back(s[i]);
+        }
+    }
+
+    if (und.size() > 0) {
+        if (sum % 10 == 0) {
+            sort(all(und));
+            sum -= und[0];
+        }
+    } else {
+        sum = 0;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
