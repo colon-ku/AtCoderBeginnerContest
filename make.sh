90e82a1f3ade80e38f3b42b1bfba98b4571b94fb
colon-ku <ksmtsho3@gmail.com>
2020-12-22 00:23:10 +0900

    addurl.sh make.shを高速化

--- make.sh
+++ make.sh
@@ -1,10 +1,15 @@
 #!/bin/zsh
 dir=$1
 
+expr "$dir" + 1 >&/dev/null
+stat=$?
+
 if [ "xxxxx$dir" = "xxxxx" ] ; then
     echo "Type the contest number of which you are participating."
 elif [ -e $dir ] ; then
     echo "Directory named \"$dir\" exists."
+elif [ $stat -ge 2 ] ; then
+    echo "Type a number."
 else
     mkdir $dir
     cp tmp.cpp "${dir}/a.cpp"
@@ -12,5 +17,5 @@ else
     cp tmp.cpp "${dir}/c.cpp"
     cp tmp.cpp "${dir}/d.cpp"
 
-    zsh ./addurl.sh
+    zsh ./addurl.sh $dir
 fi
